a88de8942ea94f4ef7771ca5dd827b21d0535a59
Jesús Navarrete <jesus.navarrete@gmail.com>
2009-04-20 12:56:46 +0200

    fixing url in mail and send only one mail by page created

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -19,25 +19,23 @@ module HelpHelper
     link_to(name, cms_connect_path(link_page), options)
   end 
   
-  def create_tree(route, link_page)     
+  def create_tree(route, link_page)
     route_z = route.gsub('/', '_')
     routes = route.split('/').reject(&:empty?)
     page = Page.find_by_slug(route_z)
     
     if page == nil
       routes.each_index do |index|
+
         dir = Page.find_by_slug(routes[index])
         if dir == nil && !(routes.last == routes[index])
           parent = (routes[index] == routes.first ? 'help' : routes[index - 1])
           page_from_parent(routes[index], routes[index], parent)
-          #Notifier.deliver_created_new_page(link_page)
-          send_internal_message(link_page)
         elsif dir == nil && (routes.last == routes[index])
           page_from_parent(routes[index], route_z, routes[index - 1])
-          #Notifier.deliver_created_new_page(link_page)          
-          send_internal_message(link_page)          
         end      
       end
+      send_internal_message(request.protocol + request.host_with_port + link_page)
     end
   end
   
@@ -58,7 +56,7 @@ module HelpHelper
           :from => current_user,
           :to => user,
           :subject => Tog::Config["plugins.tog_core.mail.default_subject"] + I18n.t("tog_help.helpers.help_helper.subject"),      
-          :content => I18n.t("tog_help.notifier.mailer.hi") + '<br/>' + I18n.t("tog_help.notifier.mailer.text") + '<br/>' +
+          :content => '<br/>' + I18n.t("tog_help.notifier.mailer.text") + '<br/>' +
                       link_to(page_url, page_url) + '<br/>' + 
                       I18n.t("tog_help.notifier.mailer.bye") 
       )
